ef5382bc45d4c4abae96c033e40eca92949aeb13
schemesmith <tbo221@lehigh.edu>
2021-07-04 12:42:02 -0700

    correct creation of folders and images in database

--- uploadFiles.py
+++ uploadFiles.py
@@ -8,8 +8,20 @@ path = ('path for folders')
 for folder in os.listdir(path):
     print("FILES IN FOLDER: ",folder)
     print("==============================")
+    urls='http://127.0.0.1:8000/uploadFolder'
+    r=requests.post(urls, data =  {"folder":folder, "sex":folder[1]})
+    print(r.status_code)
+    print(folder[1])
     for image in os.listdir(path+folder):
+        index = int(image[7:9])
+        
+        #if folder == "WM_219":
+            
         print(image)
+        f = open(path+folder+'/'+image, 'rb')
+        urls='http://127.0.0.1:8000/uploadMass'
+        r=requests.post(urls, files= {'imageFile':f}, data =  {"folder":folder, "filename":image, "index":index})
+        print(r.status_code)
 
 
 #f = open(path, 'rb')
